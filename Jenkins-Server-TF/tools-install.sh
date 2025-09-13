483cc63a9cc429c8dc3eacd66dc254cdca2131fb
udjain23 <uditsinghai22@gmail.com>
2025-09-13 23:41:46 +0530

    Update tools-install.sh

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -7,13 +7,13 @@ sudo apt install openjdk-17-jdk -y
 java --version
 
 # Installing Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+#curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+#  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+#echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#  https://pkg.jenkins.io/debian binary/ | sudo tee \
+#  /etc/apt/sources.list.d/jenkins.list > /dev/null
+#sudo apt-get update -y
+#sudo apt-get install jenkins -y
 
 # Installing Docker 
 #!/bin/bash
@@ -25,7 +25,7 @@ sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
 
 # If you don't want to install Jenkins, you can create a container of Jenkins
-# docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins/jenkins:lts
+docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins/jenkins:lts
 
 # Run Docker Container of Sonarqube
 #!/bin/bash
